87755e79a138cf26cdcb046b7f9491fe86665b4a
AnkushM123 <ankushm@incubxperts.com>
2024-02-15 18:59:35 +0530

    request ui

--- src/core/services/update-service.js
+++ src/core/services/update-service.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import path from '../../config/path';
 
-const updateUser = async (id, formData) => {
+const updateUser = async (id, formData,jwtToken) => {
     return axios.put(path.update + `/${id}`, formData, {
         headers: {
             'Content-Type': 'multipart/form-data',
+            'Authorization' : `Bearer ${jwtToken}`
         },
     })
 }
